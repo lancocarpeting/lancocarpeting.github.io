f3571503945578d745587395de9c7829076466c4
Contra <contra@maricopa.edu>
2013-07-10 16:59:26 -0700

    analytics

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-$(function(){
-  $("#emailButton").click(function(e){
+$(function () {
+  $("#emailButton").click(function (e) {
     e.preventDefault();
 
     var firstName = $("#firstName").val();
@@ -12,31 +12,32 @@ $(function(){
     if (!subject || subject.length === 0) return;
     if (!msg || msg.length === 0) return;
 
-    subject = subject+" ("+firstName+" "+lastName+")";
-    var mailtoUrl = "mailto:LancoCarpetCleaning1@hotmail.com﻿?subject="+subject+"&body="+msg;
+    subject = subject + " (" + firstName + " " + lastName + ")";
+    var mailtoUrl = "mailto:LancoCarpetCleaning1@hotmail.com﻿?subject=" +
+      subject + "&body=" + msg;
     window.location.href = mailtoUrl;
   });
 });
 
-  goog_snippet_vars = function() {
-    var w = window;
-    w.google_conversion_id = 996898759;
-    w.google_conversion_label = "mrA6CJGC8wQQx--t2wM";
-    w.google_conversion_value = 1;
-  }
-  // DO NOT CHANGE THE CODE BELOW.
-  goog_report_conversion = function(url) {
-    goog_snippet_vars();
-    window.google_conversion_format = "3";
-    window.google_is_call = true;
-    var opt = new Object();
-    opt.onload_callback = function() {
-    if (typeof(url) != 'undefined') {
+goog_snippet_vars = function () {
+  var w = window;
+  w.google_conversion_id = 996898759;
+  w.google_conversion_label = "mrA6CJGC8wQQx--t2wM";
+  w.google_conversion_value = 1;
+}
+// DO NOT CHANGE THE CODE BELOW.
+goog_report_conversion = function (url) {
+  goog_snippet_vars();
+  window.google_conversion_format = "3";
+  window.google_is_call = true;
+  var opt = new Object();
+  opt.onload_callback = function () {
+    if (typeof (url) != 'undefined') {
       window.location = url;
     }
   }
   var conv_handler = window['google_trackConversion'];
-  if (typeof(conv_handler) == 'function') {
+  if (typeof (conv_handler) == 'function') {
     conv_handler(opt);
   }
 }
